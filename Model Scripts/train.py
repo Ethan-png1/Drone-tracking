19b35e87a68129ce2cd94e1d3e50e4dce3bc257b
Ethan <emac411292e@gmail.com>
2025-10-23 21:37:15 -0400

    Trained new models to test of the model-testing code

--- Model Scripts/train.py
+++ Model Scripts/train.py
@@ -1,14 +1,14 @@
 from ultralytics import YOLO
 
 def main():
-    model = YOLO("yolo11n.pt")
+    model = YOLO("yolov9t.pt")
 
     results = model.train(
         data="../custom_data.yaml", 
         epochs=50, 
         imgsz=640,
-        project="../Models/runs/train",
-        name="drone_detector_v1"
+        project="../Models/runs",
+        name="drone_detector_v3"
     )
 
 if __name__ =='__main__':
