3322fdca771fd6dbb0081ce0774035f0b9543d85
Ethan <emac411292e@gmail.com>
2025-11-09 19:52:18 -0500

    training update for the baseline compare

--- Model Scripts/train.py
+++ Model Scripts/train.py
@@ -1,17 +1,34 @@
 from ultralytics import YOLO
 
-def main():
-    model = YOLO("yolo11n.pt")
+def train(intial_model, model_name):
+    model = YOLO(intial_model)
 
     results = model.train(
         data="../custom_data.yaml", 
-        epochs=50, 
+        epochs= 75, 
         imgsz=640,
+
+        batch=-1,                  
+        workers=8,                  
+        optimizer='AdamW',
+
         project="../Models/runs",
-        name="GigaDrone1"
+        name=model_name
     )
+
+def main():
+    
+    model_matrix = [
+        ['yolo11n.pt', 'yolo11n_compare'],
+        ['yolov10n.pt', 'yolov10n_compare']
+    ]
+
+    for model in model_matrix:
+        initial_model_path = model[0]
+        output_name = model[1]
+        
+        train(initial_model_path, output_name)
+    
 
 if __name__ =='__main__':
     main()
-
-
