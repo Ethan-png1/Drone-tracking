b5b5f8878ccfb9e8a05f7a564ea10e94508a9e3f
Ethan <emac411292e@gmail.com>
2026-02-14 14:40:26 -0500

    Made the best.pt for 89k run into onnx format. It is found in Model Scripts

---
+++ Model Scripts/changeFormat.py
@@ -0,0 +1,18 @@
+from ultralytics import YOLO
+
+def main():
+    model = YOLO('best.pt')
+
+    model.export(
+        format='onnx', 
+        half=True, 
+        simplify=True,
+        imgsz=640,
+        opset=12,
+        dynamic=False,
+        data='custom_data.yaml'
+    )
+    
+
+if __name__=='__main__':
+    main()
